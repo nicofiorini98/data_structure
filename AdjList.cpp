bcc758cae9e6e743787ca668796af8a7bbb2425d
nico <nico@nico.homenet.telecomitalia.it>
2021-02-07 13:27:38 +0100

    allocazione dinamica risolta

--- AdjList.cpp
+++ AdjList.cpp
@@ -2,31 +2,65 @@
 
 AdjList::AdjList(std::vector<Edge> const &_edges): Graph()
 {
-
+    
     for(auto &edge: _edges){
         //if the adjList don't have the src node then add the adjList in the adjLists
-        std::vector<std::list<Node>>::iterator it_src = getList(*edge.src);
-        std::vector<std::list<Node>>::iterator it_dest = getList(*edge.dest);
+        std::vector<std::list<Node*>>::iterator itr_src = getList(*edge.src);
+        std::vector<std::list<Node*>>::iterator itr_dest = getList(*edge.dest);
+
+        Node *src;
+        Node *dest;
+
+        {
+            //create node src if doesn't exist else get the pointer of the node in the list
+            if(itr_src == adj_lists.end())
+            {
+                src=new Node(*edge.src);
+                //std::cout<<"creazione nodo sorgente: "<<src->value<<" in memoria: "<<src<<"\n";
+            }
+            else
+                src = *itr_src->begin();
+
+            //create node dest if doesn't exist else get the pointer of the node in the list
+            if(itr_dest == adj_lists.end())
+            {
+                dest = new Node(*edge.dest);
+                //std::cout<<"creazione nodo dest: "<<dest->value<<" in memoria: "<<dest<<"\n";
+            }
+            else 
+                dest= *itr_dest->begin();
+        }
 
         //add node lists if it doesn't exist in adjList else add only an edge in the list of the node 
-        if(it_src == adj_lists.end()){
-            adj_lists.push_back({*edge.src,*edge.dest}); //TODO
+        if(itr_src == adj_lists.end() && itr_dest==adj_lists.end())
+        {
+            adj_lists.push_back({src,dest}); 
+            adj_lists.push_back({dest});
+            num_node+=2;
+            num_edge++;
+            //std::cout <<" aggiunto arco" <<src->value<<" ," << dest->value<<"\n";
+        }
+        else if(itr_src == adj_lists.end()){
+            adj_lists.push_back({src,dest}); 
             num_node++;
             num_edge++;
         }
-        else{
-            (*it_src).push_back({*edge.dest}); 
+        else
+        {
+            (*itr_src).push_back({dest}); 
             num_edge++;
         }
          
         //add the destination edge if isn't in the adjList
-        if(it_dest==adj_lists.end()){
-            adj_lists.push_back({*edge.dest});
+        /*if(itr_dest==adj_lists.end()){
+            adj_lists.push_back({dest});
             num_node++;
         }
+
+        */
+        
     }
     generate_key();
-    //std::cout<<"il grado del nodo "<<(*adj_lists[1].begin()).value<<" è :" <<grade((*adj_lists[1].begin()).pos)<<"\n";
 
 }
 
@@ -37,12 +71,12 @@ int AdjList::grade(int pos)
     return adj_lists[pos].size()-1;
 }
 
-std::vector<std::list<Node>>::iterator AdjList::getList(const Node &x)
+std::vector<std::list<Node*>>::iterator AdjList::getList(const Node &x)
 {
-    std::vector<std::list<Node>>::iterator itr;
+    std::vector<std::list<Node*>>::iterator itr;
 
     for(itr=adj_lists.begin();itr!=adj_lists.end();itr++)
-        if((*(*itr).begin()).value==x.value)
+        if((*(*itr).begin())->value==x.value)
             return itr;
 
     return itr;
@@ -52,7 +86,7 @@ std::vector<std::list<Node>>::iterator AdjList::getList(const Node &x)
 void AdjList::generate_key(int start)
 {
     for(int i=start;i<adj_lists.size();i++)
-        (*adj_lists[i].begin()).pos=i;
+        (*adj_lists[i].begin())->pos=i;
 }
 
  // print adjacency list representation of graph
@@ -60,15 +94,27 @@ void AdjList::showGraph() const{
 
     for (auto &node: adj_lists){
         // print current vertex number
-        std::cout << (*node.begin()).pos << " --> ";
+        std::cout << (*node.begin())<< " --> ";
         // print all neighboring vertices of vertex i
-        for(std::list<Node>::const_iterator it = ++(node.cbegin()); it!=node.cend(); it++)
-            std::cout<<(*it).pos<<" ";
+        for(std::list<Node*>::const_iterator it = ++(node.cbegin()); it!=node.cend(); it++)
+            std::cout<<(*it)<<" ";
         
         std::cout << std::endl;
     }
 }
 
+void AdjList::showGraphValue() const{
+
+    for (auto &node: adj_lists){
+        // print current vertex number
+        std::cout << (*node.begin())->value<< " --> ";
+        // print all neighboring vertices of vertex i
+        for(std::list<Node*>::const_iterator it = ++(node.cbegin()); it!=node.cend(); it++)
+            std::cout<<(*it)->value<<" ";
+        
+        std::cout << std::endl;
+    }
+}
 //return the incident edge of the Node x
 void AdjList::getIncidentEdge(const Node &x){
      //TODO    
@@ -85,28 +131,36 @@ void AdjList::getAdjNode(const Node &x){
 //TODO Do I have to guarantee the uniqueness?
 void AdjList::addNode(const Node &x)
 {
-    std::vector<std::list<Node>>::iterator itr=getList(x);
+    std::vector<std::list<Node*>>::iterator itr=getList(x);
+
     if(itr==adj_lists.end()){
-        adj_lists.push_back({x});
-        (*adj_lists[num_node].begin()).pos=num_node;
+        Node *src=new Node(x);
+        adj_lists.push_back({src});
+        std::list<Node*> a;
+        (*adj_lists[num_node].begin())->pos=num_node;
         num_node++;
     }
+
 }
 
 //add an Edge O(n)
 void AdjList::addEdge(const Node &x,const Node &y)
 {
-    std::vector<std::list<Node>>::iterator itr=getList(x);
-    if(itr==adj_lists.end())
-    {
-        //TODO add pos in the node added
-        adj_lists.push_back({x,y});
-        num_node++;
-        num_edge++;
-    }
-    else {
-        (*itr).push_back(y);
-        num_edge++;
-    }
+    std::vector<std::list<Node*>>::iterator itr_src=getList(x);
+    std::vector<std::list<Node*>>::iterator itr_dest=getList(y);
+
+    Node *src;
+    Node *dest;
+
+    if(itr_src == adj_lists.end())
+        src=new Node(x);
+    else
+        src= *itr_src->begin();
+
+    if(itr_dest== adj_lists.end())
+        dest=new Node(y);
+    else 
+        dest= *itr_dest->begin();
+
 
 }
