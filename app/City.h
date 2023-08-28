f9278719777407b6d1788d7875e1310cb4cee6a6
flowibbia <1dnicofiorini.nf@gmail.com>
2023-08-28 19:47:22 +0200

    Refactor City

--- app/City.h
+++ app/City.h
@@ -5,7 +5,7 @@
 #include <iostream>
 
 //example for a node 
-class city{
+class City{
 	
 public: 
 
@@ -13,56 +13,56 @@ public:
     int key;
     std::string name;
 
-    city(){key=0;name="bolzano";}
-    city(int _key,const std::string& _name): key(_key),name(_name){}
+    City(){key=0;name="bolzano";}
+    City(int _key,const std::string& _name): key(_key),name(_name){}
 
     //operator =
-	city& operator=(city c){
+	City& operator=(City c){
         key = c.key;
         name = c.name;
         return *this;
     }
 
     //operator ==
-    bool operator==(const city& _s) const {
+    bool operator==(const City& _s) const {
         return key == _s.key;
     }
 
     //operator<
-    bool operator<(const city& _s) const {
+    bool operator<(const City& _s) const {
         return key < _s.key;
     }
 
     //operator<=
-    bool operator<=(const city& _s) const {
+    bool operator<=(const City& _s) const {
         return key <= _s.key;
     }
 
     //operator>
-    bool operator>(const city& _s) const {
+    bool operator>(const City& _s) const {
         return key > _s.key;
     }
 
     //operator>=
-    bool operator>=(const city& _s) const {
+    bool operator>=(const City& _s) const {
         return key >= _s.key;
     }
 
     //operator!=
-    bool operator!=(const city& _s) const {
+    bool operator!=(const City& _s) const {
         return key != _s.key;
     }
 
 
     //operator<<
-    friend std::ostream& operator<<(std::ostream& os, city c){
+    friend std::ostream& operator<<(std::ostream& os, City c){
         // os << c.key;
         os << c.name;
         return os;
     }
 
     //operator >>
-    friend std::istream& operator>>(std::istream& is, city c){
+    friend std::istream& operator>>(std::istream& is, City c){
         //todo 
         //os << c.name;
         // os>>c.key;
