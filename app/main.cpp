32818cd15bda50c5d2a043993d1886c977114cc6
flowibbia <1dnicofiorini.nf@gmail.com>
2023-10-30 15:32:07 +0100

    last???

--- app/main.cpp
+++ app/main.cpp
@@ -1,34 +1,18 @@
-#include <exception>
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <string>
 
-#include "./City.h"
-#include "GraphIncList.h"
-#include "Node.h"
-#include "TreePosVector.h"
-#include "TreePtrList.h"
-#include "DHeap.h"
 #include "Testing.h"
 #include "Dijkstra.h"
 
 
 using namespace slothLib;
 
-// output stream to print std::pair
-// template <typename T1, typename T2>
-// std::ostream &operator<<(std::ostream &os, const std::pair<T1, T2> &p) {
-//     os << p.first ;
-//     return os;
-// }
 
 int main(){
     
     std::cout<<"Benvenuto nella libreria\n";
 
-    Dijkstra();
+    // Uncomment this for trying the Dijkstra algorithm
+    // Dijkstra();
 
 
     return 0;
